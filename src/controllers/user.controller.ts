7c75f203dc124450874d50cdaa7d173fb326c1c4
Mathis Engels <mathisengels@yahoo.fr>
2021-01-07 00:17:58 +0100

    Final push v2

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import moment from "moment";
 import Controller from "./controller";
 import SUser from "../services/user.service";
 import IUser from "../interfaces/user.interface";
@@ -7,10 +8,14 @@ async function getDashboard(req: Request, res: Response): Promise<void> {
 	try {
 		const { _id } = <IUser>req.user;
 		const user = await SUser.getById(_id!);
-		const stockPrices = new Array(20).fill({ date: null, value: null });
-		for (let i = 0; i < stockPrices.length; i++) {
-			stockPrices[i].date = new Date().setSeconds(3 * i);
-			stockPrices[i].value = 5 + Math.random() * 5;
+		// @ts-ignore
+		const stockPrices = [];
+		for (let i = 0; i < 20; i++) {
+			const date = moment().seconds(i * 3);
+			stockPrices.push({
+				date: date.format("YYYY-MM-DD hh:mm:ss"),
+				value: 5 + Math.random() * 5,
+			});
 		}
 
 		return Controller.sendSuccessResponse(res, { user, stockPrices });
